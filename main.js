b6664970703f8fa473d50ff5f2297d18ac8fbe87
minorin5 <mhmmt50930@gmail.com>
2026-03-18 15:16:39 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -4,4 +4,30 @@ $(function() {
         autoplaySpeed: 1000,
         fade: true
     });
+
+    new WOW( {
+        animeteClass: 'animate_animated'
+    }).init();
+
+    $('.left').slideToggle();
+    $('.wd30').click(function() {
+        $(this).find('.color6.left').slideToggle();
+    });
+
+    $('.slideToggle').slideToggle();
+    $('.jc-more').click(function () {
+        $('.slideToggle').slideToggle();
+    });
+
+    $('.top-bear1').removeClass('top-bear1').addClass('top-bear');
+
+    // お問い合わせフォーム
+    $('.btn').click(function(){
+    $('.modal').fadeIn();
+    });
+
+    $('.close').click(function(){
+        $('.modal').fadeOut();
+    });
+    
 });
