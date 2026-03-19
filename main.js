50fabb7d33ac7dd043d4626566dd240943c499f7
minorin5 <mhmmt50930@gmail.com>
2026-03-19 13:04:17 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -21,6 +21,14 @@ $(function() {
 
     $('.top-bear1').removeClass('top-bear1').addClass('top-bear');
 
+    $('.wd33 img').hover(
+        function() {
+        $(this).addClass('wow animate__animated animate__headShake');
+    },
+        function() {
+            $(this).removeClass('wow animate__animated animate__headShake')
+        });
+
     // お問い合わせフォーム
     $('.btn').click(function(){
     $('.modal').fadeIn();
@@ -29,5 +37,34 @@ $(function() {
     $('.close').click(function(){
         $('.modal').fadeOut();
     });
+
+    //カーソル要素の指定
+  var cursor=$("#cursor");
+  
+  //mousemoveイベントでカーソル要素を移動させる
+  $(document).on("mousemove",function(e){
+               //カーソルの座標位置を取得
+    var x=e.clientX;
+    var y=e.clientY;
+    //カーソル要素のcssを書き換える用
+    cursor.css({
+      "top":y+"px",
+      "left":x+"px"
+    });
+  });
+
+  //aタグホバー
+  $("a").on({
+    "mouseenter": function() {
+      //activeクラス付与
+      cursor.addClass("active");
+      stalker.addClass("active");
+    },
+    "mouseleave": function() {
+      cursor.removeClass("active");
+      stalker.removeClass("active");
+      
+    }
+  });
     
 });
